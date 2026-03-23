d0619086e7ca220fbf9528420c8a0a8cbcfaa04c
CoderZ <948089300@qq.com>
2026-03-24 00:51:11 +0800

    feat: 创建计算器iOS原生工程
    feat: 创建计算器iOS原生工程

---
+++ CalculatorPro/CalculatorPro/CalculatorProApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorProApp.swift
+//  CalculatorPro
+//
+//  Created by mac on 2026/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
